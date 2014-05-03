4e702e033989e55bde9e6fc8b8dbe612659b7bde
Mason Smith <masonium@gmail.com>
2014-05-03 01:03:54 -0700

    Changed kbled to use WS2812 strips and 60 Hz time for updates

--- kbled/src/sketch.ino
+++ kbled/src/sketch.ino
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-SoftwareSerial SerialSoft(1, 2);
 #include <MIDI.h>
 #include <Adafruit_NeoPixel.h>
 #include <TimerOne.h>
@@ -29,43 +28,35 @@ const char NUM_COLORS = sizeof(colors) / sizeof(colors[0]);
 
 note_t NOTE_MAP[128];
 
-// Gamma correction compensates for our eyes' nonlinear perception of
-// intensity.  It's the LAST step before a pixel value is stored, and
-// allows intermediate rendering/processing to occur in linear space.
-// The table contains 256 elements (8 bit input), though the outputs are
-// only 7 bits (0 to 127).  This is normal and intentional by design: it
-// allows all the rendering code to operate in the more familiar unsigned
-// 8-bit colorspace (used in a lot of existing graphics code), and better
-// preserves accuracy where repeated color blending operations occur.
-// Only the final end product is converted to 7 bits, the native format
-// for the LPD8806 LED driver.  Gamma correction and 7-bit decimation
-// thus occur in a single operation.
-PROGMEM prog_uchar gammaTable[]  = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,
-    2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,
-    4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,
-    7,  7,  7,  8,  8,  8,  8,  9,  9,  9,  9, 10, 10, 10, 10, 11,
-   11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 15, 15, 16, 16,
-   16, 17, 17, 17, 18, 18, 18, 19, 19, 20, 20, 21, 21, 21, 22, 22,
-   23, 23, 24, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30,
-   30, 31, 32, 32, 33, 33, 34, 34, 35, 35, 36, 37, 37, 38, 38, 39,
-   40, 40, 41, 41, 42, 43, 43, 44, 45, 45, 46, 47, 47, 48, 49, 50,
-   50, 51, 52, 52, 53, 54, 55, 55, 56, 57, 58, 58, 59, 60, 61, 62,
-   62, 63, 64, 65, 66, 67, 67, 68, 69, 70, 71, 72, 73, 74, 74, 75,
-   76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
-   92, 93, 94, 95, 96, 97, 98, 99,100,101,102,104,105,106,107,108,
-  109,110,111,113,114,115,116,117,118,120,121,122,123,125,126,127
-};
+// This gamma correction table was taken from:
+// http://rgb-123.com/ws2812-color-output/
 
-// This function (which actually gets 'inlined' anywhere it's called)
-// exists so that gammaTable can reside out of the way down here in the
-// utility code...didn't want that huge table distracting or intimidating
-// folks before even getting into the real substance of the program, and
-// the compiler permits forward references to functions but not data.
+// To save bytes, the first 35 are not included and are programatically
+// generated in the 'gamma' function
+PROGMEM prog_uchar GammaE[] = {3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11,
+11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18,
+19, 19, 20, 21, 21, 22, 22, 23, 23, 24, 25, 25, 26, 27, 27, 28,
+29, 29, 30, 31, 31, 32, 33, 34, 34, 35, 36, 37, 37, 38, 39, 40,
+40, 41, 42, 43, 44, 45, 46, 46, 47, 48, 49, 50, 51, 52, 53, 54,
+55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+71, 72, 73, 74, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 88, 89,
+90, 91, 93, 94, 95, 96, 98, 99,100,102,103,104,106,107,109,110,
+111,113,114,116,117,119,120,121,123,124,126,128,129,131,132,134,
+135,137,138,140,142,143,145,146,148,150,151,153,155,157,158,160,
+162,163,165,167,169,170,172,174,176,178,179,181,183,185,187,189,
+191,193,194,196,198,200,202,204,206,208,210,212,214,216,218,220,
+222,224,227,229,231,233,235,237,239,241,244,246,248,250,252,255};
+
 inline byte gamma(byte x) {
-  return pgm_read_byte(&gammaTable[x]);
+  if (x < 22)
+    return 0;
+  else if (x < 29)
+    return 1;
+  else if (x < 35)
+    return 2;
+
+  return pgm_read_byte(&GammaE[x-35]);
 }
 
 void init_note_map()
@@ -133,10 +124,6 @@ void control_change(byte channel, byte number, byte value)
 
 void note_off(byte channel, byte pitch, byte velocity)
 {
-    // Serial.print("                              Note ");
-    // Serial.print(note_name(pitch));
-    // Serial.print(note_octave(pitch));
-    // Serial.print(" off\r\n");
     note_t si = NOTE_MAP[pitch];
     if (si.index >= 0)
     {
@@ -171,22 +158,28 @@ void note_on(byte channel, byte pitch, byte velocity)
   }
 }
 
-int currentLED = 0;
+int count = 0;
 
 void timer_interrupt()
 {
-  //for (int i = 0; i < 88; ++i)
   strip.show();
+}
+
+void setup_timer()
+{
+  // 16 Mhz processor
+  // clock prescaler of 8 (CS = 010b)
+  // 33333 for 60 Hz 
+  // CTC (cleared on clock match), (WGM=0100b)
+  Timer1.initialize( 1000000L / 60 );
+  Timer1.attachInterrupt(timer_interrupt);
 }
 
 void setup()
 {
-  SerialSoft.begin(9600);
+  Serial.begin(9600);
   init_note_map();
 
-  Timer1.initialize(1000000 / 60);
-  Timer1.attachInterrupt(timer_interrupt);
-  
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
   MIDI.setHandleNoteOn(note_on);
@@ -194,14 +187,12 @@ void setup()
   MIDI.setHandleControlChange(control_change);
 
   strip.begin();
-
-  // pinMode(7, OUTPUT);
-  // pinMode(6, OUTPUT);
+  setup_timer();
 }
 
 void loop()
 {
-    
+  cli();
   MIDI.read();
-  //strip.show();
+  sei();
 }
